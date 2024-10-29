16f073764a32b4843b7ab19514085d530775d0eb
Christopher Petito <chrisjpetito@gmail.com>
2024-10-29 10:21:18 -0700

    fix profile

--- scripts/start_basic_chat.sh
+++ scripts/start_basic_chat.sh
@@ -2,6 +2,5 @@
 
 MODEL=${1:-""}
 
-docker compose up -d
-docker compose --profile basic_chat up --build -d
-docker compose --profile basic_chat exec basic_chat python chat.py $MODEL
+docker compose --profile basic up --build -d
+docker compose --profile basic exec basic_chat python chat.py $MODEL
